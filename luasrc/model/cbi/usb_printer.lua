971c5a1862ccc8319fd7088ff07b4ed67e956830
hhzol <100747442+hhzol@users.noreply.github.com>
2026-01-26 16:57:54 +0800

    Update usb_printer.lua

--- luasrc/model/cbi/usb_printer.lua
+++ luasrc/model/cbi/usb_printer.lua
@@ -1,120 +1,128 @@
 --[[
 LuCI - Lua Configuration Interface
 
-Copyright 2008 Steven Barth
-Copyright 2005-2013 hackpascal
+Copyright 2008 Steven Barth <steven@midlink.org>
+Copyright 2005-2013 hackpascal <hackpascal@gmail.com>
 
-Licensed under the Apache License, Version 2.0
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
 ]]--
 
 require "luci.util"
 local uci = luci.model.uci.cursor_state()
 local net = require "luci.model.network"
+local netm = net.init(uci)
 
 m = Map("usb_printer", translate("USB Printer Server"),
-    translate("Shares multiple USB printers via TCP/IP.<br />" ..
-              "When modified bindings, re-plug USB connectors to take effect.<br />" ..
-              "This module requires kmod-usb-printer."))
+	translate("Shares multiple USB printers via TCP/IP.<br />When modified bingings, re-plug usb connectors to take effect.<br />This module requires kmod-usb-printer."))
 
-----------------------------------------------------------
--- 工具函数
-----------------------------------------------------------
+function hex_align(hex, num)
+	local len = num - string.len(hex)
 
-local function hex_align(hex, num)
-    local len = num - string.len(hex)
-    return string.rep("0", len) .. hex
+	return string.rep("0", len) .. hex
 end
 
-----------------------------------------------------------
--- 检测 USB 打印机
-----------------------------------------------------------
+function detect_usb_printers()
+	local data = {}
 
-local function detect_usb_printers()
-    local data = {}
-    local lps = luci.util.execi("/usr/bin/detectlp")
+	local lps = luci.util.execi("/usr/bin/detectlp")
 
-    for value in lps do
-        local row = {}
+	for value in lps do
+		local row = {}
 
-        -- detectlp 输出：
-        -- device, VID/PID/?, model, description
+		--[[
+			detectlp 的输出格式：
+			设备名，VID/PID/?,描述，型号
+		]]--
 
-        local pos = string.find(value, ",")
-        local devname = string.sub(value, 1, pos - 1)
-        value = string.sub(value, pos + 1)
+		local pos = string.find(value, ",")
 
-        pos = string.find(value, ",")
-        local product = string.sub(value, 1, pos - 1)
-        value = string.sub(value, pos + 1)
+		local devname = string.sub(value, 1, pos - 1)
 
-        pos = string.find(value, ",")
-        local model = string.sub(value, 1, pos - 1)
-        local name = string.sub(value, pos + 1)
+		local value = string.sub(value, pos + 1, string.len(value))
 
-        pos = string.find(product, "/")
-        local vid = string.sub(product, 1, pos - 1)
-        local pid = string.sub(product, pos + 1)
-        pos = string.find(pid, "/")
-        pid = string.sub(pid, 1, pos - 1)
+		pos = string.find(value, ",")
+		local product = string.sub(value, 1, pos - 1)
 
-        row.description = name
-        row.model = model
-        row.id = hex_align(vid, 4) .. ":" .. hex_align(pid, 4)
-        row.name = devname
-        row.product = product
+		value = string.sub(value, pos + 1, string.len(value))
 
-        table.insert(data, row)
-    end
+		pos = string.find(value, ",")
+		local model = string.sub(value, 1, pos - 1)
 
-    return data
+		local name = string.sub(value, pos + 1, string.len(value))
+
+		pos = string.find(product, "/");
+
+		local vid = string.sub(product, 1, pos - 1)
+
+		local pid = string.sub(product, pos + 1, string.len(product))
+
+		pos = string.find(pid, "/")
+		pid = string.sub(pid, 1, pos - 1)
+
+		row["description"] = name
+		row["model"] = model
+		row["id"] = hex_align(vid, 4) .. ":" .. hex_align(pid, 4)
+		row["name"] = devname
+		row["product"] = product
+
+		table.insert(data, row)
+	end
+
+	return data
 end
-
-----------------------------------------------------------
--- 已检测的打印机列表
-----------------------------------------------------------
 
 local printers = detect_usb_printers()
 
 v = m:section(Table, printers, translate("Detected printers"))
+
 v:option(DummyValue, "description", translate("Description"))
 v:option(DummyValue, "model", translate("Printer Model"))
 v:option(DummyValue, "id", translate("VID/PID"))
 v:option(DummyValue, "name", translate("Device Name"))
 
-----------------------------------------------------------
--- 绑定配置
-----------------------------------------------------------
-
-local netm = net.init(m.uci)
+net = net.init(m.uci)
 
 s = m:section(TypedSection, "printer", translate("Bindings"))
 s.addremove = true
 s.anonymous = true
 
-s:option(Flag, "enabled", translate("Enable"))
+s:option(Flag, "enabled", translate("enable"))
 
--- USB 设备
 d = s:option(Value, "device", translate("Device"))
 d.rmempty = true
-for _, item in ipairs(printers) do
-    d:value(item.product, item.description .. " [" .. item.id .. "]")
+
+for key, item in ipairs(printers) do
+	d:value(item["product"], item["description"] .. " [" .. item["id"] .. "]")
 end
 
--- 监听接口（★ 修复重点）
-b = s:option(ListValue, "bind", translate("Interface"),
-    translate("Specifies the interface to listen on."))
+b = s:option(ListValue, "bind", translate("Interface"), translate("Specifies the interface to listen on."))
 b.rmempty = true
 
 for _, n in ipairs(netm:get_networks()) do
     b:value(n:name(), n:name())
 end
 
--- 端口
-p = s:option(ListValue, "port", translate("Port"),
-    translate("TCP listener port.")) 
+function b.write(self, section, value)
+    local n = netm:get_network(value)
+    if n then
+        local ip = n:ipaddr() or {"0.0.0.0"}
+        Value.write(self, section, ip[1])
+    else
+        Value.write(self, section, "0.0.0.0")
+    end
+end
+
+
+p = s:option(ListValue, "port", translate("Port"), translate("TCP listener port."))
 p.rmempty = true
-for i = 9100, 9109 do
-    p:value(i, tostring(i))
+for i = 0, 9 do
+	p:value(i, 9100 + i)
 end
 
 s:option(Flag, "bidirectional", translate("Bidirectional mode"))
